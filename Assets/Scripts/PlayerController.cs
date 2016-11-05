144d2d896017a271f9a9bec923a26771828bb052
m1thr4nd1r <vitocr_santos@hotmail.com>
2016-11-05 20:35:18 -0300

    Adding basic walk and jump
    Removing unneeded files

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,20 +2,21 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+    
     Vector2 flyingVelocity;
     int speed;
     Rigidbody2D body;
-    bool flying;
+    bool flying, wallJump;
     int jumpCount;
 
 	// Use this for initialization
 	void Start () {
         body = gameObject.GetComponent<Rigidbody2D>();
         flying = false;
+        wallJump = false;
         jumpCount = 0;
         speed = 0;
-        flyingVelocity = new Vector2(1f, 0.5f);
+        flyingVelocity = new Vector2(2.5f, 0.5f);
 	}
 	
 	// Update is called once per frame
@@ -41,22 +42,32 @@ public class PlayerController : MonoBehaviour {
         {
             if (!flying && body.velocity.magnitude < 2f)
                 body.velocity += Vector2.right;
-            else if (transform.position.x <= Screen.width / 2)
+            else if (flying && Camera.main.WorldToScreenPoint(transform.position).x <= Screen.width / 2 && wallJump)
                 body.velocity = flyingVelocity;
         }
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
+            if (flying)
+                print("Flying: " + flying + "\nCalc: " + (transform.position.x <= Screen.width / 2) + "\nWallJump: " + wallJump);
+
             if (!flying && body.velocity.magnitude < 2f)
                 body.velocity += Vector2.left;
-            else if (transform.position.x > Screen.width / 2)
-                body.velocity = flyingVelocity;
+            else if (Camera.main.WorldToScreenPoint(transform.position).x > Screen.width / 2 && wallJump)
+            {
+                flying = true;
+                print("Chego");
+                flyingVelocity.Set(-flyingVelocity.x, flyingVelocity.y);
+                body.velocity = -flyingVelocity;
+            }
         }
 
         //body.velocity = speed;
         body.gravityScale = (flying) ? 0 : 1;
 
-        print("Velocity: " + body.velocity + "\nGravity: " + body.gravityScale);
+        //print((Screen.width / 2) + "|" + ;
+        //print("Flying: " + flying + "\nCalc: " + (transform.position.x <= Screen.width / 2) + "\nWallJump: " + wallJump);
+        //print("Velocity: " + body.velocity + "\nGravity: " + body.gravityScale + "\nFlying: " + flying + "\nCalc: " + (transform.position.x > Screen.width / 2));
     }
 
     void OnCollisionEnter2D(Collision2D coll)
@@ -68,5 +79,18 @@ public class PlayerController : MonoBehaviour {
         }
         if (coll.gameObject.tag.Equals("Obstacle"))
             jumpCount = 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        print("<color=green>" + coll.name + "</color>");
+        if (coll.name.Equals("Wall"))
+            wallJump = true;
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.name.Equals("Wall"))
+            wallJump = false;
     }
 }
