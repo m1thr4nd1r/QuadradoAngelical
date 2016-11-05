e79cd02911c3dafd48f6c58bcf620c695765bd5e
m1thr4nd1r <vitocr_santos@hotmail.com>
2016-11-05 17:27:47 -0300

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Vector2 flyingVelocity;
+    int speed;
+    Rigidbody2D body;
+    bool flying;
+    int jumpCount;
+
+	// Use this for initialization
+	void Start () {
+        body = gameObject.GetComponent<Rigidbody2D>();
+        flying = false;
+        jumpCount = 0;
+        speed = 0;
+        flyingVelocity = new Vector2(1f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //speed = new Vector2( Mathf.Clamp(speed.x * 0.1f, 0, 2f), Mathf.Clamp( speed.y * 0.1f, 0, 2f));
+        
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jumpCount++;
+            flying = (jumpCount > 1) ? true : false;
+
+            if (!flying)
+                body.velocity += Vector2.up * 2;
+            else
+            {
+                jumpCount = 0;
+                body.velocity = flyingVelocity;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (!flying)
+                body.velocity += Vector2.right * 2;
+            else if (transform.position.x <= Screen.width / 2)
+                body.velocity = flyingVelocity;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (!flying)
+                body.velocity += Vector2.left * 2;
+            else if (transform.position.x > Screen.width / 2)
+                body.velocity = flyingVelocity;
+        }
+
+        //body.velocity = speed;
+        body.gravityScale = (flying) ? 0 : 1;
+
+        print("Velocity: " + body.velocity + "\nGravity: " + body.gravityScale);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name.Contains("Wall"))
+        {
+            flying = false;
+            body.velocity = Vector2.zero;
+        }
+        if (coll.gameObject.tag.Equals("Obstacle"))
+            jumpCount = 0;
+    }
+}
